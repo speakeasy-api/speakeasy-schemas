07ab4fd649ae26a46a1df4e85836f049f9a1ffb5
Tristan Cartledge <tristan.cartledge@gmail.com>
2022-06-29 10:47:46 +0100

    fix: metrics model updates

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -1,16 +1,12 @@
 package metrics
 
 type ApiData struct {
-	ApiKey      string      `json:"api_key"`
-	ApiServerID string      `json:"api_server_id"`
-	Handlers    HandlerInfo `json:"handlers"`
-}
-
-type HandlerInfo struct {
-	ApiStatsByID map[uint]*ApiStats `json:"api_stats_by_id"`
+	ApiKey       string               `json:"api_key"`
+	ApiServerID  string               `json:"api_server_id"`
+	ApiStatsByID map[string]*ApiStats `json:"api_stats_by_id"`
 }
 
 type ApiStats struct {
-	NumCalls  int `json:"number_of_calls"`
-	NumErrors int `json:"number_of_errors"`
+	NumCalls  int `json:"num_calls"`
+	NumErrors int `json:"num_errors"`
 }
