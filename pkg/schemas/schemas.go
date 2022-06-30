43f673c0628764024b69e92f12019e17b6d5e91c
anuraagnalluri <anuraagnalluri@gmail.com>
2022-06-30 13:44:31 -0700

    Remove FileInfo from schema

--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -1,15 +1,7 @@
 package schemas
 
-type FileInfo struct {
-	MimeType  string // The type of the file.
-	Size      int64  // The size of the file in bytes.
-	Hash      string // The MD5 hash of the file.
-	SourceURI string // The GCS URI of the file.
-}
-
 // Schema is the shared representation of a Schema between registry and client(Speakeasy SDK).
 type Schema struct {
-	FileInfo
 	ApiID       string `json:"api_id"`
 	VersionID   string `json:"version_id"`
 	RevisionID  string `json:"revision_id"`
