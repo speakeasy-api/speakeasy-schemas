e221bd31328153c42404952a0700ca52ea6a04c1
Tristan Cartledge <tristan.cartledge@gmail.com>
2022-06-30 10:18:21 +0100

    fix: remove api key

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -1,11 +1,12 @@
 package metrics
 
+// ApiData represents data relating to a particular server and the API calls it handles
 type ApiData struct {
-	ApiKey       string               `json:"api_key"`
 	ApiServerID  string               `json:"api_server_id"`
 	ApiStatsByID map[string]*ApiStats `json:"api_stats_by_id"`
 }
 
+// ApiStats represents stats for a particular API call
 type ApiStats struct {
 	NumCalls  int `json:"num_calls"`
 	NumErrors int `json:"num_errors"`
