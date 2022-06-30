d4606c8d402e4d38c7ca7eb4c7d323be5ee50a04
anuraagnalluri <anuraagnalluri@gmail.com>
2022-06-30 13:28:37 -0700

    Modify shared api/schema models

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -1,17 +1,11 @@
 package apis
 
-import (
-	"github.com/SamuelTissot/sqltime"
-)
-
 // Api is the shared representation of an API between registry and client(Speakeasy SDK).
 type Api struct {
-	ID          uint         `gorm:"primarykey"`
-	WorkspaceID string       `json: "workspace_id"` // Uniquely identifies the workspace this Api belongs to.
-	Method      string       `json: "method"`       // HTTP verb.
-	Path        string       `json: "path"`         // Path that handles this Api.
-	CreatedAt   sqltime.Time `json: "created_at"`
-	UpdatedAt   sqltime.Time `json: "updated_at"`
-	DisplayName string       `json: "name"`                  // A human-friendly name.
-	Description string       `json: "description,omitempty"` // A detailed description.
+	ID          uint
+	WorkspaceID string `json: "workspace_id"`          // Uniquely identifies the workspace this Api belongs to.
+	Method      string `json: "method"`                // HTTP verb.
+	Path        string `json: "path"`                  // Path that handles this Api.
+	DisplayName string `json: "name"`                  // A human-friendly name.
+	Description string `json: "description,omitempty"` // A detailed description.
 }
--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -1,9 +1,5 @@
 package schemas
 
-import (
-	"github.com/SamuelTissot/sqltime"
-)
-
 type FileInfo struct {
 	MimeType  string // The type of the file.
 	Size      int64  // The size of the file in bytes.
@@ -15,12 +11,9 @@ type FileInfo struct {
 type Schema struct {
 	FileInfo
 	ID          uint   `gorm:"primarykey"`
-	ApiId       string `json:"api_id"`
-	VersionId   string `json:"version_id"`
-	RevisionId  string `json:"revision_id"`
+	ApiID       string `json:"api_id"`
+	VersionID   string `json:"version_id"`
+	RevisionID  string `json:"revision_id"`
 	Filename    string `json:"filename"`
 	Description string `json:"description"`
-	// needing to use a third pary sqltime.Time package to avoid a bug in gorm where it want handle time.Time resolution correctly
-	CreatedAt sqltime.Time `json:"created_at"`
-	UpdatedAt sqltime.Time `json:"updated_at"`
 }
