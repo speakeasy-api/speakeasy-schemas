52908f36f6c59d3283c90f4820fb18a97dfda5c7
anuraagnalluri <anuraagnalluri@gmail.com>
2022-06-30 13:48:41 -0700

    Add Api and Schema models (#1)
    * Add Api and Schema models
    
    * Modify shared api/schema models
    
    * Remove gorm primary key from schema ID
    
    * Tidy up comments
    
    * Remove ID from shared schemas
    
    * Remove FileInfo from schema

---
+++ pkg/apis/apis.go
@@ -0,0 +1,10 @@
+package apis
+
+// Api is the shared representation of an API between registry and client(Speakeasy SDK).
+type Api struct {
+	WorkspaceID string `json: "workspace_id"`          // Uniquely identifies the workspace this Api belongs to.
+	Method      string `json: "method"`                // HTTP verb.
+	Path        string `json: "path"`                  // Path that handles this Api.
+	DisplayName string `json: "name"`                  // A human-friendly name.
+	Description string `json: "description,omitempty"` // A detailed description.
+}
---
+++ pkg/schemas/schemas.go
@@ -0,0 +1,10 @@
+package schemas
+
+// Schema is the shared representation of a Schema between registry and client(Speakeasy SDK).
+type Schema struct {
+	ApiID       string `json:"api_id"`
+	VersionID   string `json:"version_id"`
+	RevisionID  string `json:"revision_id"`
+	Filename    string `json:"filename"`
+	Description string `json:"description"`
+}
