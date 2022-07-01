2e630f65ded8e3395d7f2287e7baa057a871d14e
anuraagnalluri <anuraagnalluri@gmail.com>
2022-06-30 22:00:47 -0700

    change time.Time to sqltime.Time (#3)

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -1,14 +1,16 @@
 package apis
 
-import "time"
+import (
+	"github.com/SamuelTissot/sqltime"
+)
 
 // Api is the shared representation of an API between registry and client(Speakeasy SDK).
 type Api struct {
-	WorkspaceID string    `json: "workspace_id"`          // Uniquely identifies the workspace this Api belongs to.
-	Method      string    `json: "method"`                // HTTP verb.
-	Path        string    `json: "path"`                  // Path that handles this Api.
-	DisplayName string    `json: "name"`                  // A human-friendly name.
-	Description string    `json: "description,omitempty"` // A detailed description.
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	WorkspaceID string       `json: "workspace_id"`          // Uniquely identifies the workspace this Api belongs to.
+	Method      string       `json: "method"`                // HTTP verb.
+	Path        string       `json: "path"`                  // Path that handles this Api.
+	DisplayName string       `json: "name"`                  // A human-friendly name.
+	Description string       `json: "description,omitempty"` // A detailed description.
+	CreatedAt   sqltime.Time `json:"created_at"`
+	UpdatedAt   sqltime.Time `json:"updated_at"`
 }
--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -1,13 +1,13 @@
 package schemas
 
-import "time"
+import "github.com/SamuelTissot/sqltime"
 
 // Schema is the shared representation of a Schema between registry and client(Speakeasy SDK).
 type Schema struct {
-	ApiID       string    `json:"api_id"`
-	VersionID   string    `json:"version_id"`
-	RevisionID  string    `json:"revision_id"`
-	Filename    string    `json:"filename"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
+	ApiID       string       `json:"api_id"`
+	VersionID   string       `json:"version_id"`
+	RevisionID  string       `json:"revision_id"`
+	Filename    string       `json:"filename"`
+	Description string       `json:"description"`
+	CreatedAt   sqltime.Time `json:"created_at"`
 }
