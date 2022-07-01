18da39917bcbc5c82557a9d9bdc0cd971f553039
anuraagnalluri <anuraagnalluri@gmail.com>
2022-06-30 20:23:30 -0700

    fix api

--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -1,11 +1,13 @@
 package schemas
 
+import "time"
+
 // Schema is the shared representation of a Schema between registry and client(Speakeasy SDK).
 type Schema struct {
-	ApiID       string `json:"api_id"`
-	VersionID   string `json:"version_id"`
-	RevisionID  string `json:"revision_id"`
-	Filename    string `json:"filename"`
-	Description string `json:"description"`
+	ApiID       string    `json:"api_id"`
+	VersionID   string    `json:"version_id"`
+	RevisionID  string    `json:"revision_id"`
+	Filename    string    `json:"filename"`
+	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
 }
