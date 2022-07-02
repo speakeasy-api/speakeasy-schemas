18f6a2b1e8e819c18f863346d1d43313af35873c
anuraagnalluri <anuraagnalluri@gmail.com>
2022-07-02 15:49:08 -0700

    Remove time fields from shared schemas

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/speakeasy-api/speakeasy-schemas
 
 go 1.18
-
-require github.com/SamuelTissot/sqltime v0.1.0
--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -1,13 +1,10 @@
 package schemas
 
-import "github.com/SamuelTissot/sqltime"
-
 // Schema is the shared representation of a Schema between registry and client(Speakeasy SDK).
 type Schema struct {
-	ApiID       string       `json:"api_id"`
-	VersionID   string       `json:"version_id"`
-	RevisionID  string       `json:"revision_id"`
-	Filename    string       `json:"filename"`
-	Description string       `json:"description"`
-	CreatedAt   sqltime.Time `json:"created_at"`
+	ApiID       string `json:"api_id"`
+	VersionID   string `json:"version_id"`
+	RevisionID  string `json:"revision_id"`
+	Filename    string `json:"filename"`
+	Description string `json:"description"`
 }
