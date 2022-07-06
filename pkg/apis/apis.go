a102a61fdb5fc45f0b38031a26d21499fc34cd8c
Tristan Cartledge <tristan@speakeasyapi.dev>
2022-07-06 21:37:59 +0100

    fix: add apiID to Api struct

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -2,9 +2,10 @@ package apis
 
 // Api is the shared representation of an API between registry and client(Speakeasy SDK).
 type Api struct {
-	WorkspaceID string `json: "workspace_id"`          // Uniquely identifies the workspace this Api belongs to.
-	Method      string `json: "method"`                // HTTP verb.
-	Path        string `json: "path"`                  // Path that handles this Api.
-	DisplayName string `json: "name"`                  // A human-friendly name.
-	Description string `json: "description,omitempty"` // A detailed description.
+	WorkspaceID string `json:"workspace_id"`          // Uniquely identifies the workspace this Api belongs to.
+	ApiID       string `json:"api_id"`                // Uniquely identifies an api.
+	Method      string `json:"method"`                // HTTP verb.
+	Path        string `json:"path"`                  // Path that handles this Api.
+	DisplayName string `json:"name"`                  // A human-friendly name.
+	Description string `json:"description,omitempty"` // A detailed description.
 }
