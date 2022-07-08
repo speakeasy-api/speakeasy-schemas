ce12baa63f34733e92efbdee5498c3109e3273fd
Tristan Cartledge <tristan@speakeasyapi.dev>
2022-07-08 17:16:12 +0100

    refactor: to use tags for sqlx

--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -2,9 +2,10 @@ package schemas
 
 // Schema is the shared representation of a Schema between registry and client(Speakeasy SDK).
 type Schema struct {
-	ApiID       string `json:"api_id"`
-	VersionID   string `json:"version_id"`
-	RevisionID  string `json:"revision_id"`
-	Filename    string `json:"filename"`
-	Description string `json:"description"`
+	ApiID       string `json:"api_id" db:"api_id"`
+	VersionID   string `json:"version_id" db:"version_id"`
+	SchemaID    string `json:"schema_id" db:"schema_id"`
+	RevisionID  string `json:"revision_id" db:"revision_id"`
+	Filename    string `json:"filename" db:"filename"`
+	Description string `json:"description" db:"description"`
 }
