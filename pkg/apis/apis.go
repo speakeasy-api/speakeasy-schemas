9a947edd404796b6fd4fe1bbe95c5e468dd4c615
Tristan Cartledge <tristan@speakeasyapi.dev>
2022-07-11 11:08:38 +0100

    refactor: update apis model

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -2,10 +2,10 @@ package apis
 
 // Api is the shared representation of an API between registry and client(Speakeasy SDK).
 type Api struct {
-	WorkspaceID string `json:"workspace_id"`          // Uniquely identifies the workspace this Api belongs to.
-	ApiID       string `json:"api_id"`                // Uniquely identifies an api.
-	Method      string `json:"method"`                // HTTP verb.
-	Path        string `json:"path"`                  // Path that handles this Api.
-	DisplayName string `json:"name"`                  // A human-friendly name.
-	Description string `json:"description,omitempty"` // A detailed description.
+	WorkspaceID string `json:"workspace_id" db:"workspace_id"`         // Uniquely identifies the workspace this Api belongs to.
+	ApiID       string `json:"api_id" db:"api_id"`                     // Uniquely identifies an api.
+	Method      string `json:"method" db:"method"`                     // HTTP verb.
+	Path        string `json:"path" db:"path"`                         // Path that handles this Api.
+	DisplayName string `json:"name" db:"name"`                         // A human-friendly name.
+	Description string `json:"description,omitempty" db:"description"` // A detailed description.
 }
